139c0b9af30ac7a085efd30adc770568aeda6cb1
Riley McCloskey <rsmccc@iastate.edu>
2015-11-10 22:34:30 -0600

    datasets updates

--- code/analysis.py
+++ code/analysis.py
@@ -5,3 +5,10 @@ Created on Mon Nov 04 22:12:21 2015
 @authors: Riley McCloskey, Michael Tuttle, Will Smith, Miranda Fisher
 """
 
+import requests
+import pandas as pd
+import xlrd
+
+wb = xlrd.open_workbook('../data/SuperstoreSales.xlsx')
+
+df = pd.read_excel(wb, sheetname='Orders', index_col='Order Date', engine='xlrd')
